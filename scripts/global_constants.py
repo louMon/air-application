fee82aaf5c43d1f3146388541a208e95b80b054f
Lourdes Montalvo <loumontalvolujan@gmail.com>
2022-03-19 22:13:40 -0500

    update number of pools

--- scripts/global_constants.py
+++ scripts/global_constants.py
@@ -8,7 +8,7 @@ last_hours_historical_interpolate = 24
 #Global constanst temporal prediction
 time_steps_in = 24
 
-pool_size_interpolate = 1
+pool_size_interpolate = 8
 k_value=5
 name_column_x = 'lon'
 name_column_y = 'lat'
