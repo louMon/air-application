2b6ca3ec235a7012f43a788c32b47f16653fbd59
Lourdes Montalvo <loumontalvolujan@gmail.com>
2022-06-20 13:47:37 -0500

    8 concurrent

--- scripts/global_constants.py
+++ scripts/global_constants.py
@@ -8,8 +8,8 @@ last_hours_historical_interpolate = 24
 #Global constanst temporal prediction
 time_steps_in = 24
 
-pool_size_interpolate = 7
-k_value=4
+pool_size_interpolate = 8
+k_value=5
 name_column_x = 'lon'
 name_column_y = 'lat'
 dictionary_of_var_index_prediction= {'CO': 0,'NO2': 1, 'PM25': 2}
